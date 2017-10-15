657e7b869cc2a4ed3da3c1b7dbd9e31c8872857d
tormozz48 <tormozz48@gmail.com>
2017-10-15 21:31:44 +0300

    Implement iterator pattern

--- behavioral/iterator/index.js
+++ behavioral/iterator/index.js
@@ -1,1 +1,46 @@
 'use strict';
+
+class Iterator {
+    constructor(collection) {
+        this._collection = collection;
+        this._index = 0;
+    }
+
+    get collection() {
+        return this._collection;
+    }
+
+    first() {
+        this.reset();
+        return this.next();
+    }
+
+    reset() {
+        this._index = 0;
+    }
+
+    next() {
+        return this.collection[this._index++];
+    }
+
+    hasNext() {
+        return this._index < this.collection.length;
+    }
+
+    iterate(callback) {
+        this.reset();
+
+        while(this.hasNext()) {
+            callback(this.next())
+        }
+    }
+}
+
+// demonstation
+
+const iterator = new Iterator(['apple', 'peach', 'melon']);
+
+console.log(iterator.first());
+console.log(iterator.next());
+
+iterator.iterate(console.log);

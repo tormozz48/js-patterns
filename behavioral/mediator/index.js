66fda5b667e61a79c545af641bd009516280f311
tormozz48 <tormozz48@gmail.com>
2017-11-26 10:43:18 +0300

    Implement pattern mediator

--- behavioral/mediator/index.js
+++ behavioral/mediator/index.js
@@ -1,1 +1,48 @@
 'use strict';
+
+class Airplane {
+    constructor(id) {
+        this.id = id;
+    }
+
+    linkToTower(tower) {
+        this._dispatchTower = tower;
+        return this;
+    }
+
+    takeOff() {
+        this._dispatchTower.notify('takeOf', this);
+    }
+
+    landing() {
+        this._dispatchTower.notify('landing', this);
+    }
+
+    updateInfo(event, id) {
+        console.info(`airplane ${this.id} receive info: board ${id} is ${event}`);
+    }
+}
+
+class DispatchTower {
+    constructor(airplanes) {
+        this._airplanes = airplanes.map((plane) => plane.linkToTower(this));
+    }
+
+    notify(event, airplane) {
+        this._airplanes
+            .filter((_airplane) => _airplane.id !== airplane.id)
+            .forEach((_airplane) => _airplane.updateInfo(event, airplane.id));
+    }
+}
+
+// demonstration
+
+const airplane1 = new Airplane(1);
+const airplane2 = new Airplane(2);
+const airplane3 = new Airplane(3);
+
+const dispatchTower = new DispatchTower([airplane1, airplane2, airplane3]);
+
+airplane1.landing();
+airplane2.takeOff();
+airplane1.takeOff();
